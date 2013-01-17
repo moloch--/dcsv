e383055416cfa4db971788eb5b7c755ac9bcc4ba
moloch <moloch38@gmail.com>
2013-01-16 19:16:58 -0800

    Added option to extract column

--- dcsv.py
+++ dcsv.py
@@ -45,15 +45,18 @@ def parse_csv(file_path, verbose=False):
         display_info("Rendering, please wait...\n")
     return table
 
-def extract_csv(field_name, file_path, verbose=False):
+def extract_csv(file_path, field_name, verbose=False):
     results = []
     with open(file_path, 'rb') as csvfile:
         reader = csv.reader(csvfile,  dialect=csv.excel)
         for index, row in enumerate(reader):
             if index == 0:
-                field_index = row.index(field_name)
-                assert 0 <= field_index
-            else:
+                try:
+                    field_index = row.index(field_name)
+                except ValueError:
+                    print WARN + '%s is an invalid column name.' % field_name
+                    os._exit(5)
+            elif 0 < len(row):
                 results.append(row[field_index])
     return results
 
@@ -72,6 +75,19 @@ def save_table(fpath, table, start=0, end=None, fields=None, html=False, verbose
         else:
             outfile.write(table.get_string(start=start, end=end, fields=fields) + '\n')
     if verbose: 
+        display_info('Saving data to %s ... done!\n' % fpath)
+
+def save_data(fpath, data, start=0, end=None, verbose=False):
+    if verbose: 
+        display_info('Saving data to %s ...' % fpath)
+    if end is None:
+        save = data[start:]
+    else:
+        save = data[start:end]
+    with open(fpath, 'w') as outfile:
+        for line in save:
+            outfile.write('%s\n' % line)
+    if verbose:
         display_info('Saving data to %s ... done!\n' % fpath)
 
 if __name__ == '__main__':
@@ -122,34 +138,66 @@ if __name__ == '__main__':
         dest="html", 
         help="Write output file as HTML",
         default=False)
+    parser.add_option(
+        "-x", "--extract",
+        dest="extract", 
+        help="Pull a single column",
+        default=None)
     (options, args) = parser.parse_args()
     if len(args) == 0:
         print WARN + 'Not enough arguments, see --help'
         os._exit(1)
     elif not os.path.exists(args[0]):
-    	print WARN + 'File does not exist:', os.path.abspath(args[0])
-    	os._exit(2)
+        print WARN + 'File does not exist:', os.path.abspath(args[0])
+        os._exit(2)
     if options.verbose: 
         display_info('Reading %s, please wait...\n' % args[0])
-    table = parse_csv(args[0], verbose=options.verbose)
+    if options.extract is not None:
+        extracted_data = extract_csv(args[0], options.extract, options.verbose)
+    else:
+        table = parse_csv(args[0], verbose=options.verbose)
     if options.console:
-        if options.header:
-            for name in table.field_names: 
-                print '\t%s' % name
-        elif options.filter is not None:
-            filter_names = []
-            for name in options.filter.split(","):
-                if name not in table.field_names and options.verbose:
-                    print WARN + "Invalid column name:", name
-                elif name in table.field_names:
-                    filter_names.append(name)
-            print table.get_string(start=options.start, end=options.end, fields=filter_names)
+        if options.extract is not None:
+            for index, line in enumerate(extracted_data):
+                print "%d. %s" % (index + 1, line)
         else:
-            print table.get_string(start=options.start, end=options.end)
+            if options.header:
+                for index, name in enumerate(table.field_names): 
+                    print '%d. %s' % (index +1, name)
+            elif options.filter is not None:
+                filter_names = []
+                for name in options.filter.split(","):
+                    if name not in table.field_names and options.verbose:
+                        print WARN + "Invalid column name:", name
+                    elif name in table.field_names:
+                        filter_names.append(name)
+                if options.end is not None:
+                    print table.get_string(
+                        start=options.start, 
+                        end=options.end, 
+                        fields=filter_names)
+                else:
+                    print table.get_string(
+                        start=options.start, 
+                        fields=filter_names)
+            else:
+                if options.end is not None: 
+                    print table.get_string(
+                        start=options.start, 
+                        end=options.end)
+                else: 
+                    print table.get_string(start=options.start)
+    # Save to file if requested
     if options.file is not None:
-        save_table(options.file, table, 
-            start=options.start, 
-            end=options.end,
-            fields=options.filter,
-            html=options.html,
-            verbose=options.verbose)
+        if options.extract is not None:
+            save_data(options.file, extracted_data, 
+                start=options.start, 
+                end=options.end, 
+                verbose=options.verbose)
+        else:
+            save_table(options.file, table, 
+                start=options.start, 
+                end=options.end,
+                fields=options.filter,
+                html=options.html,
+                verbose=options.verbose)
